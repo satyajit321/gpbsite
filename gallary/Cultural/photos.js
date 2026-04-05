72674d0557e4fab0b5876d6999d9f4b223272452
Satyajit321 <67871005+satyajit321@users.noreply.github.com>
2026-04-05 13:45:44 +0530

    Add files via upload

---
+++ gallary/Cultural/photos.js
@@ -0,0 +1,80 @@
+WPGgallery = new Object();
+WPGgallery.name = "Adobe%20Web%20Photo%20Gallery";
+WPGgallery.photographer = "";
+WPGgallery.contact = "";
+WPGgallery.email = "gpbaroowah@gmail.com";
+WPGgallery.date = "8/19/2008"
+
+WPGgallery.colors = new Object();
+WPGgallery.colors.background = "#FFFFFF";
+WPGgallery.colors.banner = "#F0F0F0";
+WPGgallery.colors.text = "#000000";
+WPGgallery.colors.link = "#0000FF";
+WPGgallery.colors.alink = "#FF0000";
+WPGgallery.colors.vlink = "#800080";
+
+gPhotos = new Array();
+gPhotos[0] = new Object();
+gPhotos[0].filename = "cul.jpg";
+gPhotos[0].ImageWidth = 350;
+gPhotos[0].ImageHeight = 262;
+gPhotos[0].ThumbWidth = 75;
+gPhotos[0].ThumbHeight = 56;
+gPhotos[0].meta = new Object();
+
+gPhotos[1] = new Object();
+gPhotos[1].filename = "cul%20(1).jpg";
+gPhotos[1].ImageWidth = 350;
+gPhotos[1].ImageHeight = 262;
+gPhotos[1].ThumbWidth = 75;
+gPhotos[1].ThumbHeight = 56;
+gPhotos[1].meta = new Object();
+
+gPhotos[2] = new Object();
+gPhotos[2].filename = "cul%20(2).jpg";
+gPhotos[2].ImageWidth = 350;
+gPhotos[2].ImageHeight = 262;
+gPhotos[2].ThumbWidth = 75;
+gPhotos[2].ThumbHeight = 56;
+gPhotos[2].meta = new Object();
+
+gPhotos[3] = new Object();
+gPhotos[3].filename = "cul%20(3).jpg";
+gPhotos[3].ImageWidth = 350;
+gPhotos[3].ImageHeight = 262;
+gPhotos[3].ThumbWidth = 75;
+gPhotos[3].ThumbHeight = 56;
+gPhotos[3].meta = new Object();
+
+gPhotos[4] = new Object();
+gPhotos[4].filename = "cul%20(4).jpg";
+gPhotos[4].ImageWidth = 350;
+gPhotos[4].ImageHeight = 262;
+gPhotos[4].ThumbWidth = 75;
+gPhotos[4].ThumbHeight = 56;
+gPhotos[4].meta = new Object();
+
+gPhotos[5] = new Object();
+gPhotos[5].filename = "cul%20(5).jpg";
+gPhotos[5].ImageWidth = 350;
+gPhotos[5].ImageHeight = 262;
+gPhotos[5].ThumbWidth = 75;
+gPhotos[5].ThumbHeight = 56;
+gPhotos[5].meta = new Object();
+
+gPhotos[6] = new Object();
+gPhotos[6].filename = "cul%20(6).jpg";
+gPhotos[6].ImageWidth = 350;
+gPhotos[6].ImageHeight = 262;
+gPhotos[6].ThumbWidth = 75;
+gPhotos[6].ThumbHeight = 56;
+gPhotos[6].meta = new Object();
+
+gPhotos[7] = new Object();
+gPhotos[7].filename = "cul%20(7).jpg";
+gPhotos[7].ImageWidth = 350;
+gPhotos[7].ImageHeight = 262;
+gPhotos[7].ThumbWidth = 75;
+gPhotos[7].ThumbHeight = 56;
+gPhotos[7].meta = new Object();
+
