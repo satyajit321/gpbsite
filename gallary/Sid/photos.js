9a615ec1ab2a3949c22d2467eaf3c971120b9ea0
Satyajit321 <67871005+satyajit321@users.noreply.github.com>
2026-04-05 14:34:54 +0530

    Add files via upload

---
+++ gallary/Sid/photos.js
@@ -0,0 +1,152 @@
+WPGgallery = new Object();
+WPGgallery.name = "Adobe%20Web%20Photo%20Gallery";
+WPGgallery.photographer = "";
+WPGgallery.contact = "";
+WPGgallery.email = "gpbaroowah@gmail.com";
+WPGgallery.date = "8/21/2008"
+
+WPGgallery.colors = new Object();
+WPGgallery.colors.background = "#FFFFFF";
+WPGgallery.colors.banner = "#F0F0F0";
+WPGgallery.colors.text = "#000000";
+WPGgallery.colors.link = "#0000FF";
+WPGgallery.colors.alink = "#FF0000";
+WPGgallery.colors.vlink = "#800080";
+
+gPhotos = new Array();
+gPhotos[0] = new Object();
+gPhotos[0].filename = "Picture%20035.jpg";
+gPhotos[0].ImageWidth = 350;
+gPhotos[0].ImageHeight = 286;
+gPhotos[0].ThumbWidth = 75;
+gPhotos[0].ThumbHeight = 61;
+gPhotos[0].meta = new Object();
+
+gPhotos[1] = new Object();
+gPhotos[1].filename = "Picture%20040.jpg";
+gPhotos[1].ImageWidth = 350;
+gPhotos[1].ImageHeight = 286;
+gPhotos[1].ThumbWidth = 75;
+gPhotos[1].ThumbHeight = 61;
+gPhotos[1].meta = new Object();
+
+gPhotos[2] = new Object();
+gPhotos[2].filename = "sid.jpg";
+gPhotos[2].ImageWidth = 233;
+gPhotos[2].ImageHeight = 350;
+gPhotos[2].ThumbWidth = 50;
+gPhotos[2].ThumbHeight = 75;
+gPhotos[2].meta = new Object();
+
+gPhotos[3] = new Object();
+gPhotos[3].filename = "sid%20(1).jpg";
+gPhotos[3].ImageWidth = 350;
+gPhotos[3].ImageHeight = 262;
+gPhotos[3].ThumbWidth = 75;
+gPhotos[3].ThumbHeight = 56;
+gPhotos[3].meta = new Object();
+
+gPhotos[4] = new Object();
+gPhotos[4].filename = "sid%20(10).jpg";
+gPhotos[4].ImageWidth = 350;
+gPhotos[4].ImageHeight = 262;
+gPhotos[4].ThumbWidth = 75;
+gPhotos[4].ThumbHeight = 56;
+gPhotos[4].meta = new Object();
+
+gPhotos[5] = new Object();
+gPhotos[5].filename = "sid%20(11).jpg";
+gPhotos[5].ImageWidth = 350;
+gPhotos[5].ImageHeight = 262;
+gPhotos[5].ThumbWidth = 75;
+gPhotos[5].ThumbHeight = 56;
+gPhotos[5].meta = new Object();
+
+gPhotos[6] = new Object();
+gPhotos[6].filename = "sid%20(12).jpg";
+gPhotos[6].ImageWidth = 350;
+gPhotos[6].ImageHeight = 262;
+gPhotos[6].ThumbWidth = 75;
+gPhotos[6].ThumbHeight = 56;
+gPhotos[6].meta = new Object();
+
+gPhotos[7] = new Object();
+gPhotos[7].filename = "sid%20(13).jpg";
+gPhotos[7].ImageWidth = 350;
+gPhotos[7].ImageHeight = 262;
+gPhotos[7].ThumbWidth = 75;
+gPhotos[7].ThumbHeight = 56;
+gPhotos[7].meta = new Object();
+
+gPhotos[8] = new Object();
+gPhotos[8].filename = "sid%20(14).jpg";
+gPhotos[8].ImageWidth = 350;
+gPhotos[8].ImageHeight = 262;
+gPhotos[8].ThumbWidth = 75;
+gPhotos[8].ThumbHeight = 56;
+gPhotos[8].meta = new Object();
+
+gPhotos[9] = new Object();
+gPhotos[9].filename = "sid%20(2).jpg";
+gPhotos[9].ImageWidth = 350;
+gPhotos[9].ImageHeight = 262;
+gPhotos[9].ThumbWidth = 75;
+gPhotos[9].ThumbHeight = 56;
+gPhotos[9].meta = new Object();
+
+gPhotos[10] = new Object();
+gPhotos[10].filename = "sid%20(3).jpg";
+gPhotos[10].ImageWidth = 350;
+gPhotos[10].ImageHeight = 262;
+gPhotos[10].ThumbWidth = 75;
+gPhotos[10].ThumbHeight = 56;
+gPhotos[10].meta = new Object();
+
+gPhotos[11] = new Object();
+gPhotos[11].filename = "sid%20(4).jpg";
+gPhotos[11].ImageWidth = 350;
+gPhotos[11].ImageHeight = 262;
+gPhotos[11].ThumbWidth = 75;
+gPhotos[11].ThumbHeight = 56;
+gPhotos[11].meta = new Object();
+
+gPhotos[12] = new Object();
+gPhotos[12].filename = "sid%20(5).jpg";
+gPhotos[12].ImageWidth = 350;
+gPhotos[12].ImageHeight = 262;
+gPhotos[12].ThumbWidth = 75;
+gPhotos[12].ThumbHeight = 56;
+gPhotos[12].meta = new Object();
+
+gPhotos[13] = new Object();
+gPhotos[13].filename = "sid%20(6).jpg";
+gPhotos[13].ImageWidth = 350;
+gPhotos[13].ImageHeight = 262;
+gPhotos[13].ThumbWidth = 75;
+gPhotos[13].ThumbHeight = 56;
+gPhotos[13].meta = new Object();
+
+gPhotos[14] = new Object();
+gPhotos[14].filename = "sid%20(7).jpg";
+gPhotos[14].ImageWidth = 350;
+gPhotos[14].ImageHeight = 262;
+gPhotos[14].ThumbWidth = 75;
+gPhotos[14].ThumbHeight = 56;
+gPhotos[14].meta = new Object();
+
+gPhotos[15] = new Object();
+gPhotos[15].filename = "sid%20(8).jpg";
+gPhotos[15].ImageWidth = 350;
+gPhotos[15].ImageHeight = 262;
+gPhotos[15].ThumbWidth = 75;
+gPhotos[15].ThumbHeight = 56;
+gPhotos[15].meta = new Object();
+
+gPhotos[16] = new Object();
+gPhotos[16].filename = "sid%20(9).jpg";
+gPhotos[16].ImageWidth = 350;
+gPhotos[16].ImageHeight = 262;
+gPhotos[16].ThumbWidth = 75;
+gPhotos[16].ThumbHeight = 56;
+gPhotos[16].meta = new Object();
+
